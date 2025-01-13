1d9d0363e1ac93474e226fa5ee661e7ca1332ea7
Orion Moonclaw <killer@fawx.news>
2025-01-13 19:22:11 +0100

    input: make raw pose match SteamVR
    This uses values from SteamVR rendermodel files, just like OpenComposite
    With SteamVR skeletal data this fixes #30

--- src/input/profiles/knuckles.rs
+++ src/input/profiles/knuckles.rs
@@ -94,15 +94,35 @@ impl InteractionProfile for Knuckles {
         }
     }
 
-    fn offset_grip_pose(&self, mut pose: xr::Posef) -> xr::Posef {
-        let rot = glam::Quat::from_rotation_x(-FRAC_PI_6);
-        pose.orientation = xr::Quaternionf {
-            x: rot.x,
-            y: rot.y,
-            z: rot.z,
-            w: rot.w,
-        };
-        pose
+    fn offset_grip_pose(&self, hand: Hand) -> xr::Posef {
+        match hand {
+            Hand::Left => xr::Posef {
+                orientation: xr::Quaternionf {
+                    x: -0.133942,
+                    y: 0.017555,
+                    z: -0.005040,
+                    w: 0.990821,
+                },
+                position: xr::Vector3f {
+                    x: -0.004619,
+                    y: -0.020021,
+                    z: -0.129239,
+                },
+            },
+            Hand::Right => xr::Posef {
+                orientation: xr::Quaternionf {
+                    x: -0.133942,
+                    y: -0.017555,
+                    z: 0.005040,
+                    w: 0.990821,
+                },
+                position: xr::Vector3f {
+                    x: 0.004619,
+                    y: -0.020021,
+                    z: -0.129239,
+                },
+            },
+        }
     }
 }
 
